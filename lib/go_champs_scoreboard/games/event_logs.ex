b1abf7d964beca630dd072e9e3d5fb66a52cc905
Lair Junior <lairjr@outlook.com>
2025-04-23 01:23:36 -0300

    Add basic functionality to operate EventLog

--- lib/go_champs_scoreboard/games/event_logs.ex
+++ lib/go_champs_scoreboard/games/event_logs.ex
@@ -2,6 +2,7 @@ defmodule GoChampsScoreboard.Games.EventLogs do
   alias GoChampsScoreboard.Events.EventLog
   alias GoChampsScoreboard.Repo
   alias GoChampsScoreboard.Events.Models.Event
+  import Ecto.Query
 
   @spec persist(Event.t()) :: :ok | {:error, any()}
   def persist(event) do
@@ -13,5 +14,67 @@ defmodule GoChampsScoreboard.Games.EventLogs do
       timestamp: event.timestamp
     })
     |> Repo.insert()
+  end
+
+  @spec insert_after_event(Ecto.UUID.t(), Event.t()) :: {:ok, EventLog.t()} | {:error, any()}
+  def insert_after_event(prior_event_id, event) do
+    prior_event = Repo.get(EventLog, prior_event_id)
+
+    if prior_event do
+      after_timestamp = DateTime.add(prior_event.timestamp, 1, :microsecond)
+
+      %EventLog{}
+      |> EventLog.changeset(%{
+        key: event.key,
+        game_id: event.game_id,
+        payload: event.payload,
+        timestamp: after_timestamp,
+        prior_event_id: prior_event.id
+      })
+      |> Repo.insert()
+    else
+      {:error, :prior_event_not_found}
+    end
+  end
+
+  @spec update_payload(Ecto.UUID.t(), map()) :: {:ok, EventLog.t()} | {:error, any()}
+  def update_payload(id, new_payload) do
+    event_log = get(id)
+
+    case event_log do
+      nil ->
+        {:error, :not_found}
+
+      _ ->
+        event_log
+        |> EventLog.changeset(%{payload: new_payload})
+        |> Repo.update()
+    end
+  end
+
+  @spec get(Ecto.UUID.t()) :: EventLog.t() | nil
+  def get(id) do
+    EventLog
+    |> Repo.get(id)
+  end
+
+  @spec delete(Ecto.UUID.t()) :: {:ok, EventLog.t()} | {:error, any()}
+  def delete(id) do
+    event_log = get(id)
+
+    case event_log do
+      nil -> {:error, :not_found}
+      _ -> Repo.delete(event_log)
+    end
+  end
+
+  @spec get_all_by_game_id(Ecto.UUID.t()) :: [EventLog.t()]
+  def get_all_by_game_id(game_id) do
+    query =
+      from e in EventLog,
+        where: e.game_id == ^game_id,
+        order_by: [asc: e.timestamp]
+
+    Repo.all(query)
   end
 end
--- test/go_champs_scoreboard/games/event_logs_test.exs
+++ test/go_champs_scoreboard/games/event_logs_test.exs
@@ -28,8 +28,194 @@ defmodule GoChampsScoreboard.Games.EventLogsTest do
                "stat-id" => "field_goals_made"
              }
 
+      assert event_log.id != nil
       assert event_log.inserted_at != nil
       assert event_log.updated_at != nil
+    end
+  end
+
+  describe "get/1" do
+    test "retrieves the event log by ID" do
+      update_player_stat_event =
+        GoChampsScoreboard.Events.Definitions.UpdatePlayerStatDefinition.create(
+          "7488a646-e31f-11e4-aace-600308960668",
+          %{
+            "operation" => "increment",
+            "team-type" => "home",
+            "player-id" => "123",
+            "stat-id" => "field_goals_made"
+          }
+        )
+
+      {:ok, event_log} = EventLogs.persist(update_player_stat_event)
+      retrieved_event_log = EventLogs.get(event_log.id)
+
+      assert retrieved_event_log.id == event_log.id
+      assert retrieved_event_log.key == event_log.key
+      assert retrieved_event_log.game_id == event_log.game_id
+      assert retrieved_event_log.payload == event_log.payload
+    end
+
+    test "returns nil for non-existent event log" do
+      assert EventLogs.get("7488a646-e31f-11e4-aace-600308960668") == nil
+    end
+  end
+
+  describe "update_payload/2" do
+    test "updates the event log payload by ID" do
+      update_player_stat_event =
+        GoChampsScoreboard.Events.Definitions.UpdatePlayerStatDefinition.create(
+          "7488a646-e31f-11e4-aace-600308960668",
+          %{
+            "operation" => "increment",
+            "team-type" => "home",
+            "player-id" => "123",
+            "stat-id" => "field_goals_made"
+          }
+        )
+
+      {:ok, event_log} = EventLogs.persist(update_player_stat_event)
+
+      updated_payload = %{
+        "operation" => "decrement",
+        "team-type" => "away",
+        "player-id" => "456",
+        "stat-id" => "rebounds"
+      }
+
+      {:ok, updated_event_log} = EventLogs.update_payload(event_log.id, updated_payload)
+
+      assert updated_event_log.payload == updated_payload
+    end
+
+    test "returns error for non-existent event log" do
+      assert EventLogs.update_payload("7488a646-e31f-11e4-aace-600308960668", %{}) ==
+               {:error, :not_found}
+    end
+  end
+
+  describe "insert_after_event" do
+    test "inserts an event after a specific event" do
+      update_player_stat_event =
+        GoChampsScoreboard.Events.Definitions.UpdatePlayerStatDefinition.create(
+          "7488a646-e31f-11e4-aace-600308960668",
+          %{
+            "operation" => "increment",
+            "team-type" => "home",
+            "player-id" => "123",
+            "stat-id" => "field_goals_made"
+          }
+        )
+
+      {:ok, event_log} = EventLogs.persist(update_player_stat_event)
+
+      new_event =
+        GoChampsScoreboard.Events.Definitions.UpdateClockTimeAndPeriodDefinition.create(
+          "7488a646-e31f-11e4-aace-600308960668",
+          %{
+            "property" => "time",
+            "operation" => "increment"
+          }
+        )
+
+      {:ok, inserted_event_log} = EventLogs.insert_after_event(event_log.id, new_event)
+
+      assert inserted_event_log.key == new_event.key
+      assert inserted_event_log.game_id == new_event.game_id
+      assert inserted_event_log.payload == new_event.payload
+      assert inserted_event_log.timestamp == DateTime.add(event_log.timestamp, 1, :microsecond)
+    end
+
+    test "returns error for non-existent event log" do
+      new_event = %{
+        key: "new-event",
+        game_id: "7488a646-e31f-11e4-aace-600308960668",
+        payload: %{"new_key" => "new_value"},
+        timestamp: DateTime.utc_now()
+      }
+
+      assert EventLogs.insert_after_event("7488a646-e31f-11e4-aace-600308960668", new_event) ==
+               {:error, :prior_event_not_found}
+    end
+  end
+
+  describe "delete/1" do
+    test "deletes the event log by ID" do
+      update_player_stat_event =
+        GoChampsScoreboard.Events.Definitions.UpdatePlayerStatDefinition.create(
+          "7488a646-e31f-11e4-aace-600308960668",
+          %{
+            "operation" => "increment",
+            "team-type" => "home",
+            "player-id" => "123",
+            "stat-id" => "field_goals_made"
+          }
+        )
+
+      {:ok, event_log} = EventLogs.persist(update_player_stat_event)
+      assert EventLogs.get(event_log.id) != nil
+
+      EventLogs.delete(event_log.id)
+      assert EventLogs.get(event_log.id) == nil
+    end
+  end
+
+  describe "get_all_by_game_id/1" do
+    test "retrieves all event logs for a specific game ID sorted by timestamp" do
+      game_id = "7488a646-e31f-11e4-aace-600308960668"
+
+      event1 =
+        GoChampsScoreboard.Events.Definitions.UpdatePlayerStatDefinition.create(
+          game_id,
+          %{
+            "operation" => "increment",
+            "team-type" => "home",
+            "player-id" => "123",
+            "stat-id" => "field_goals_made"
+          }
+        )
+
+      event3 =
+        GoChampsScoreboard.Events.Definitions.UpdateClockTimeAndPeriodDefinition.create(
+          game_id,
+          %{
+            "property" => "time",
+            "operation" => "increment"
+          }
+        )
+
+      event2 =
+        GoChampsScoreboard.Events.Definitions.UpdatePlayerStatDefinition.create(
+          game_id,
+          %{
+            "operation" => "decrement",
+            "team-type" => "away",
+            "player-id" => "456",
+            "stat-id" => "rebounds"
+          }
+        )
+
+      event4 =
+        GoChampsScoreboard.Events.Definitions.UpdateClockTimeAndPeriodDefinition.create(
+          game_id,
+          %{
+            "property" => "period",
+            "operation" => "increment"
+          }
+        )
+
+      {:ok, event1} = EventLogs.persist(event1)
+      {:ok, event2} = EventLogs.persist(event2)
+      {:ok, event3} = EventLogs.insert_after_event(event1.id, event3)
+      {:ok, event4} = EventLogs.persist(event4)
+
+      event_logs = EventLogs.get_all_by_game_id(game_id)
+
+      assert length(event_logs) == 4
+      assert Enum.at(event_logs, 0).id == event1.id
+      assert Enum.at(event_logs, 1).id == event3.id
+      assert Enum.at(event_logs, 2).id == event2.id
+      assert Enum.at(event_logs, 3).id == event4.id
     end
   end
 end
