ef08354ad237facee6d6f3602b7ad4926c10c2d2
Lair Junior <lairjr@outlook.com>
2025-11-04 18:52:21 -0300

    Fix timeout issue

--- assets/js/components/basketball_5x5/ClockControls.tsx
+++ assets/js/components/basketball_5x5/ClockControls.tsx
@@ -100,7 +100,7 @@ function InGameClockControls({
     <div className="columns is-multiline">
       <div className="column is-4">
         <TimeoutButton
-          teamType="away"
+          teamType="home"
           disabled={clockButtonsDisabled}
           pushEvent={pushEvent}
         />
@@ -112,7 +112,7 @@ function InGameClockControls({
 
       <div className="column is-4">
         <TimeoutButton
-          teamType="home"
+          teamType="away"
           disabled={clockButtonsDisabled}
           pushEvent={pushEvent}
         />
