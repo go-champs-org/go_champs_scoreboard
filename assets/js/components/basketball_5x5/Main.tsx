b0344984c741434526955da4eec001c17cca0303
Lair Junior <lairjr@outlook.com>
2026-01-13 15:02:56 +0000

    Add Medium Plus top level

--- assets/js/components/basketball_5x5/Main.tsx
+++ assets/js/components/basketball_5x5/Main.tsx
@@ -4,6 +4,7 @@ import LiveEndedModal from './LiveEndedModal';
 import { BASKETBALL_VIEWS } from './constants';
 import { useSelectedView } from '../../shared/ViewSettingsContext';
 import MediumView from './Views/MediumView';
+import MediumPlusView from './Views/MediumPlusView';
 import BasicView from './Views/BasicView';
 
 export interface LiveReactBase {
@@ -24,16 +25,28 @@ function Main({ game_state, recent_events, pushEvent }: MainProps) {
 
   return (
     <>
-      {selectedView === BASKETBALL_VIEWS.BASIC ? (
-        <BasicView
+      {selectedView === BASKETBALL_VIEWS.MEDIUM && (
+        <MediumView
           game_state={game_state}
           recent_events={recent_events}
           pushEvent={pushEvent}
           selection={selection}
           setSelection={setSelection}
         />
-      ) : (
-        <MediumView
+      )}
+
+      {selectedView === BASKETBALL_VIEWS.MEDIUM_PLUS && (
+        <MediumPlusView
+          game_state={game_state}
+          recent_events={recent_events}
+          pushEvent={pushEvent}
+          selection={selection}
+          setSelection={setSelection}
+        />
+      )}
+
+      {selectedView === BASKETBALL_VIEWS.BASIC && (
+        <BasicView
           game_state={game_state}
           recent_events={recent_events}
           pushEvent={pushEvent}
